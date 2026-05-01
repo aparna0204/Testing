65c3a8b5faf252aa5cf820f6d56e4c17b9a2e2ca
Aparna K <aparnas.kanumalla@gmail.com>
2026-05-01 07:57:29 +0530

    latest code changes of ai reviewer is testing

--- src/main/java/com/example/test/TestingApplication.java
+++ src/main/java/com/example/test/TestingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingApplication {
 
 	public static void main(String[] args) {
-		  System.out.println("Hello, World!...........");
+		  System.out.println("Hello, World! This is a test application.");
 		  
 		SpringApplication.run(TestingApplication.class, args);
 	}
