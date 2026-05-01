3abcd45a58645397944dc19606f4c0f36d4f8d8c
Aparna K <aparnas.kanumalla@gmail.com>
2026-05-01 05:56:06 +0530

    Added Helloworld

--- src/main/java/com/example/test/TestingApplication.java
+++ src/main/java/com/example/test/TestingApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingApplication {
 
 	public static void main(String[] args) {
+		  System.out.println("Hello, World!");
+		
 		SpringApplication.run(TestingApplication.class, args);
 	}
 
