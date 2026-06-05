78b1ccc60aa2a4bac55b8b16c92a7a902e94e88c
Aparna Kanumalla <aparnas.kanumalla@gmail.com>
2026-06-05 10:04:40 +0530

    latest changes

--- src/main/java/com/example/test/TestingApplication.java
+++ src/main/java/com/example/test/TestingApplication.java
@@ -8,6 +8,8 @@ public class TestingApplication {
 
 	public static void main(String[] args) {
 		String s = null;
+		String gitToken = "12345678";
+		
 		if(s.length() > 0) {
 		  System.out.println("Hello World! This is a test12 application."+ s.length());
 		  System.out.println("Testing the application over here on 0406-10"+ s.length());
