fdca74d4dce43d3d86c3f21504fa3e11a743c8b3
Aparna Kanumalla <aparnas.kanumalla@gmail.com>
2026-06-07 18:07:37 +0530

    changes

--- src/main/java/com/example/test/TestingApplication.java
+++ src/main/java/com/example/test/TestingApplication.java
@@ -7,18 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingApplication {
 
 	public static void main(String[] args) {
-		String s = null;
-		String gitToken = "123456788972345678899987654321312344324";
-		
-		
-		if(s.length() > 0) {
-		  System.out.println("Hello World! This is a test12 application."+ s.length());
-		  System.out.println("Testing the application over here on 0406-10"+ s.length());
-		  
-		}
-		  
-		  System.out.println("Testing GitHub API integration...over the call testing");
-		  
 		  
 		SpringApplication.run(TestingApplication.class, args);
 	}
