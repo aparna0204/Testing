fdc3cd498b5f8db1eb95309a6b0d3b4dda2b981f
Aparna Kanumalla <aparnas.kanumalla@gmail.com>
2026-06-18 23:35:37 +0530

    changes

--- src/main/java/com/example/test/TestingApplication.java
+++ src/main/java/com/example/test/TestingApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestingApplication {
 	public static void main(String[] args) {
-		// Removed null reference to prevent NullPointerException
-		String s = "test";
+		String s = null;
 		String password = "1234567889723456788999876543278687134546789687685gjhfsgjvnvn";
-		if(s != null && s.length() > 0) {
+		if( s.length() > 0) {
 		  System.out.println("Hello World! This is a test12 application."+ s.length());
 		  System.out.println("Testing the application over here on 0406-10"+ s.length());
 		  
